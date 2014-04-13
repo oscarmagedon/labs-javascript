6e729218749c9fb7f8c485723997b0bbff2dacc0
Oscar D Vegas V <oscarmagedon@gmail.com>
2014-04-13 02:01:56 -0400

    redoing

--- labs/Generals/main.php
+++ labs/Generals/main.php
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<div style="width: 700px">
-    <p class="text-wrapper">
-        En este laboratorio haremos un paseo por las mas importantes funcionalidades
-        de javascript en un modo Basico y entendible.
-    </p>
-    <p class="text-wrapper">
-        <b>Para mayor referencia</b> ir a la <a href="http://www.w3schools.com/js/js_examples.asp">W3C Schools</a>
-        donde se muestran todas las referencias que se desean sobre javascript.
-    </p>
-    <p class="text-wrapper">
-        La idea, creacion y concepto de este lab es identico al de CSS y Jquery. Solo que la idea es que se sepa
-        cual es el trabajo de Javascript y como Jquery ha mejorado su forma compleja de acceder a los elementos
-        de un HTML normal.
-    </p>
-    <p class="text-wrapper">
-        Feedback! Importante, este lab en especifico, es de PROGRAMACION, asi que si se encuentra alguna
-        dificultad en el camino es parte natural del proceso y por eso es importante el feedback sobre si el LAB
-        ayuda o no a la investigacion y puesta en practica.
-    </p>
-</div>
-=======
 <div style="width: 80%">
     <h2 class="first-title">Hello programming world</h2>
 
@@ -57,4 +35,3 @@
         <li><a href="http://www.w3schools.com/js/js_syntax.asp" target="_blank">JS Syntax</a></li>
     </ul>
 </div>
->>>>>>> fb3145a062961a9733802af01a5f8a909c462461
